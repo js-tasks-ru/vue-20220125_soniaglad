8275b65988bb607b1904ba35dad5f97360c7d385
Sofya Gladkikh <sg@soniaglad.com>
2022-02-01 16:05:05 +0300

    professor's solution seen

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -3,25 +3,34 @@ import { createApp } from './vendor/vue.esm-browser.js';
 const app = createApp({
   data() {
     return {
-      x: 13,
-      y: 5,
-      result: 0,
-    }
+      x: null,
+      y: null,
+      result: {
+        sum: 'sum',
+        subtract: 'subtract',
+        multiply: 'multiply',
+        divide: 'divide',
+      },
+    };
   },
   computed: {
-      sum() {
-        return this.result = this.x + this.y;
-      },
-      subtract() {
-        return this.result = this.x - this.y;
-      },
-      multiply() {
-        return this.result = this.x * this.y;
-      },
-      divide() {
-        return this.result = this.x / this.y;
-      },
-  }
+    culcResult() {
+      // ???
+    },
+    /*
+    sum() {
+      return (this.result = this.x + this.y);
+    },
+    subtract() {
+      return (this.result = this.x - this.y);
+    },
+    multiply() {
+      return (this.result = this.x * this.y);
+    },
+    divide() {
+      return (this.result = this.x / this.y);
+    },*/
+  },
 });
 
 const vm = app.mount('#app');
