956289943311fa3e3ac0dfb4be9f14304b1916bc
Sofya Gladkikh <sg@soniaglad.com>
2022-02-15 18:28:51 +0300

    Lesson 2: task 1

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,28 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+
+    emits: ['update:count'],
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
+  methods: {
+    count() {
+      return this.count + 1;
+    },
+  },
+
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button
+                type="button"
+                @click='$emit("update:count", $event.target.value)'
+                :count='count'>
+                {{ count }}
+            </button>`,
 });
